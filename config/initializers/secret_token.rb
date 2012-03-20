b1fee17904616929c8bebde1ae68e61f015e8dc5
Tick Tock <ticktock>
2012-03-19 22:44:03 -0400

    new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cen4010Scheduler::Application.config.secret_token = '8f79c4abb0f51e860bec4c1d275cb1dc19092020c0cf880e2517944cde71d6ed0c4634c75000282bb21d5d14a96f3cd0adb9a7113277ac0dadc5d3521d16fe27'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Cen4010Scheduler::Application.config.session_store :cookie_store, key: '_cen4010-scheduler_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Cen4010Scheduler::Application.config.session_store :active_record_store
