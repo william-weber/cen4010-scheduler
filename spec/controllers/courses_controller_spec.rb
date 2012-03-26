90df612ed58b31261f301fa98dc29311b6efd20e
Sandy Weber <sandy.j.weber@gmail.com>
2012-03-25 20:52:34 -0400

    removed useless test

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -7,15 +7,6 @@ describe CoursesController do
       get 'index'
       response.should be_success
     end
-
-    it "with no params, assigns list of courses for current semester" do
-      courses = []
-      courses << Factory( :course, :offering_pattern => 1 )
-      Factory( :course, :offering_pattern => 2 )
-      courses << Factory( :course, :offering_pattern => 1 )
-      get 'index'
-      assigns(:courses).should =~ courses
-    end
   end
 
 end
